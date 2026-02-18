a9f908f69bb70fe28a67d5638fe3a29870121e63
Anshika1378 <anshikaagarwal56890@gmail.com>
2026-02-18 15:15:38 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import { AuthButton } from '../components/AuthButton';
 
 export default function Home() {
   return (
-    <div className="text-center mt-10">
-      <h2 className="text-2xl font-semibold">
-        Welcome to Smart Bookmark App
-      </h2>
-      <p className="mt-4">Login with Google to manage your bookmarks.</p>
-      <div className="mt-6 flex justify-center">
-        <AuthButton />  
-      </div>
-    </div>
+    <div className="max-w-md mx-auto text-center mt-10 border-2 p-10 rounded-lg shadow-lg">
+  <h2 className="text-2xl font-semibold">
+    Welcome to Smart Bookmark App
+  </h2>
+  <p className="mt-4">Login with Google to manage your bookmarks.</p>
+  <div className="mt-6 flex justify-center">
+    <AuthButton />  
+  </div>
+</div>
+
   );
 }
